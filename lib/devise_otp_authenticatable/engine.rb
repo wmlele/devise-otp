e5bbdec19c6c53a8d3409192d5bf4a296873f618
Nicolas Rodriguez <nico@nicoladmin.fr>
2022-02-19 15:41:29 +0100

    Wrap calls to ActiveSupport hooks in *initializer* method

--- lib/devise_otp_authenticatable/engine.rb
+++ lib/devise_otp_authenticatable/engine.rb
@@ -1,22 +1,23 @@
 module DeviseOtpAuthenticatable
   class Engine < ::Rails::Engine
 
-    ActiveSupport.on_load(:action_controller) do
-      include DeviseOtpAuthenticatable::Controllers::UrlHelpers
-      include DeviseOtpAuthenticatable::Controllers::Helpers
-    end
-
-    ActiveSupport.on_load(:action_view) do
-      include DeviseOtpAuthenticatable::Controllers::UrlHelpers
-      include DeviseOtpAuthenticatable::Controllers::Helpers
-    end
-
     # We use to_prepare instead of after_initialize here because Devise is a Rails engine;
     config.to_prepare do
       DeviseOtpAuthenticatable::Hooks.apply
     end
 
     initializer "devise-otp", group: :all do |app|
+      ActiveSupport.on_load(:action_controller) do
+        include DeviseOtpAuthenticatable::Controllers::UrlHelpers
+        include DeviseOtpAuthenticatable::Controllers::Helpers
+      end
+
+      ActiveSupport.on_load(:action_view) do
+        include DeviseOtpAuthenticatable::Controllers::UrlHelpers
+        include DeviseOtpAuthenticatable::Controllers::Helpers
+      end
+
+      # See: https://guides.rubyonrails.org/engines.html#separate-assets-and-precompiling
       # check if Rails api mode
       if app.config.respond_to?(:assets)
         if defined?(Sprockets) && Sprockets::VERSION >= "4"
