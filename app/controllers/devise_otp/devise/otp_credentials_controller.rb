cd019fb9759ec0b7f1b1c407269fc7ad154f9fac
Dalen Ward <dalen.ward@m6securities.com>
2023-02-20 15:50:45 -0700

    whoops

--- app/controllers/devise_otp/devise/otp_credentials_controller.rb
+++ app/controllers/devise_otp/devise/otp_credentials_controller.rb
@@ -39,61 +39,61 @@ module DeviseOtp
         if token.blank?
           otp_set_flash_message(:alert, :token_blank)
           redirect_to otp_credential_path_for(resource_name, challenge: params[resource_name][:challenge],
-            recovery: recovery)
+                                              recovery: recovery)
         elsif resource.nil?
           otp_set_flash_message(:alert, :otp_session_invalid)
           redirect_to new_session_path(resource_name)
         elsif resource.otp_challenge_valid? && resource.validate_otp_token(params[resource_name][:token], recovery)
           sign_in(resource_name, resource)
 
-            otp_set_trusted_device_for(resource) if (params[:enable_persistence] == 'true') || (params[:enable_persistence] == 'on') || (params[:enable_persistence] == 'checked')
-            otp_refresh_credentials_for(resource)
-            respond_with resource, :location => after_sign_in_path_for(resource)
-          else
-            otp_set_flash_message :alert, :token_invalid
-            redirect_to new_session_path(resource_name)
-          end
-        end
-      end
-
-      #
-      # displays the request for a credentials refresh
-      #
-      def get_refresh
-        ensure_resource!
-        render :refresh
-      end
-
-      #
-      # lets the user through is the refresh is valid
-      #
-      def set_refresh
-        ensure_resource!
-
-        if resource.valid_password?(params[resource_name][:refresh_password])
-          done_valid_refresh
+          otp_set_trusted_device_for(resource) if (params[:enable_persistence] == 'true') || (params[:enable_persistence] == 'on') || (params[:enable_persistence] == 'checked')
+          otp_refresh_credentials_for(resource)
+          respond_with resource, :location => after_sign_in_path_for(resource)
         else
-          failed_refresh
+          otp_set_flash_message :alert, :token_invalid
+          redirect_to new_session_path(resource_name)
         end
       end
+    end
 
-      private
+    #
+    # displays the request for a credentials refresh
+    #
+    def get_refresh
+      ensure_resource!
+      render :refresh
+    end
 
-      def done_valid_refresh
-        otp_refresh_credentials_for(resource)
-        otp_set_flash_message :success, :valid_refresh if is_navigational_format?
+    #
+    # lets the user through is the refresh is valid
+    #
+    def set_refresh
+      ensure_resource!
 
-        respond_with resource, location: otp_fetch_refresh_return_url
+      if resource.valid_password?(params[resource_name][:refresh_password])
+        done_valid_refresh
+      else
+        failed_refresh
       end
+    end
 
-      def failed_refresh
-        otp_set_flash_message :alert, :invalid_refresh
-        render :refresh
-      end
+    private
 
-      def self.controller_path
-        "#{::Devise.otp_controller_path}/otp_credentials"
-      end
+    def done_valid_refresh
+      otp_refresh_credentials_for(resource)
+      otp_set_flash_message :success, :valid_refresh if is_navigational_format?
+
+      respond_with resource, location: otp_fetch_refresh_return_url
+    end
+
+    def failed_refresh
+      otp_set_flash_message :alert, :invalid_refresh
+      render :refresh
+    end
+
+    def self.controller_path
+      "#{::Devise.otp_controller_path}/otp_credentials"
     end
   end
 end
+
