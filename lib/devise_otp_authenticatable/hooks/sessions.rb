12d5372cb66d92d40c2113f9eb83a64fb4829154
Brock Williams <brock@cottonwoodcomputer.com>
2023-06-29 15:15:46 -0600

    fix login with mandatory otp set

--- lib/devise_otp_authenticatable/hooks/sessions.rb
+++ lib/devise_otp_authenticatable/hooks/sessions.rb
@@ -25,7 +25,7 @@ module DeviseOtpAuthenticatable::Hooks
         store_location_for(resource, devise_stored_location) # restore the stored location
         respond_with resource, location: otp_credential_path_for(resource, {challenge: challenge})
       elsif otp_mandatory_on?(resource) # if mandatory, log in user but send him to the must activate otp
-        set_flash_message(:notice, :signed_in_but_otp) if is_navigational_format?
+        #set_flash_message(:notice, :signed_in_but_otp) if is_navigational_format?
         sign_in(resource_name, resource)
         respond_with resource, location: otp_token_path_for(resource)
       else
@@ -49,9 +49,8 @@ module DeviseOtpAuthenticatable::Hooks
     # the resource -should- have otp turned on, but it isn't
     #
     def otp_mandatory_on?(resource)
-      return true if resource.class.otp_mandatory
+      return true if resource.class.otp_mandatory && !resource.otp_enabled
       return false unless resource.respond_to?(:otp_mandatory)
-
       resource.otp_mandatory && !resource.otp_enabled
     end
   end
