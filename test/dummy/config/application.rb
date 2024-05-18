0323538b952d9677c8ce564d665fa3e90170b5fd
Laney Stroup <laney@stroupsolutions.com>
2024-05-18 12:47:19 +0900

    remove legacy_connection_handling from test application (deprecated in Rails 7.0 and removed in Rails 7.1);

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -53,8 +53,6 @@ module Dummy
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
-    config.active_record.legacy_connection_handling = false
-
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
