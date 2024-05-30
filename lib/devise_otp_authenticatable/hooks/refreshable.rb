8919fa6280819c65610fba8f760de4b22bc782cd
Laney Stroup <laney@stroupsolutions.com>
2024-05-31 02:10:46 +0900

    update Refreshable hook and helper methods to get/set values as strings again (rather than symbols);

--- lib/devise_otp_authenticatable/hooks/refreshable.rb
+++ lib/devise_otp_authenticatable/hooks/refreshable.rb
@@ -1,5 +1,5 @@
 # After each sign in, update credentials refreshed at time
 Warden::Manager.after_set_user except: :fetch do |record, warden, options|
-  warden.session[:credentials_refreshed_at] = (Time.now + record.class.otp_credentials_refresh)
+  warden.session["credentials_refreshed_at"] = (Time.now + record.class.otp_credentials_refresh)
 end
 
