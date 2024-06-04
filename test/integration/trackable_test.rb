34a71544e99c519abad2e2a2b2f6456a88e9e432
Laney Stroup <laney@stroupsolutions.com>
2024-06-04 21:04:47 +0900

    add test to ensure normal trackable functionality for disabled OTP;

--- test/integration/trackable_test.rb
+++ test/integration/trackable_test.rb
@@ -2,37 +2,49 @@ require "test_helper"
 require "integration_tests_helper"
 
 class TrackableTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = sign_user_in
+
+    @user.reload
+
+    @sign_in_count = @user.sign_in_count
+    @current_sign_in_at = @user.current_sign_in_at
+
+    sign_out
+  end
+
   def teardown
     Capybara.reset_sessions!
   end
 
-  test "should not update devise trackable fields until user enters user token" do
-    user = sign_user_in
+  test "if otp is disabled, it should update devise trackable fields as usual when the user signs in" do
+    sign_user_in(@user)
 
-    user.reload
+    @user.reload
 
-    sign_in_count = user.sign_in_count
-    current_sign_in_at = user.current_sign_in_at
+    assert_not_equal @sign_in_count, @user.sign_in_count
+    assert_not_equal @current_sign_in_at, @user.current_sign_in_at
+  end
 
-    sign_out
+  test "if otp is enabled, it should not update devise trackable fields until user enters their user token to complete their sign in" do
+    @user.populate_otp_secrets!
+    @user.enable_otp!
 
-    user.populate_otp_secrets!
-    user.enable_otp!
+    sign_user_in(@user)
 
-    sign_user_in(user)
+    @user.reload
 
-    user.reload
+    assert_equal @sign_in_count, @user.sign_in_count
+    assert_equal @current_sign_in_at, @user.current_sign_in_at
 
-    assert_equal sign_in_count, user.sign_in_count
-    assert_equal current_sign_in_at, user.current_sign_in_at
-
-    fill_in "user_token", with: ROTP::TOTP.new(user.otp_auth_secret).at(Time.now)
+    fill_in "user_token", with: ROTP::TOTP.new(@user.otp_auth_secret).at(Time.now)
     click_button "Submit Token"
 
-    user.reload
+    @user.reload
 
-    assert_not_equal sign_in_count, user.sign_in_count
-    assert_not_equal current_sign_in_at, user.current_sign_in_at
+    assert_not_equal @sign_in_count, @user.sign_in_count
+    assert_not_equal @current_sign_in_at, @user.current_sign_in_at
   end
 
 end
