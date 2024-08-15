ef05170a48b8f0fbb3fdcec87c225d94ed8f63a2
Brandon Hicks <tarellel@gmail.com>
2024-08-14 22:19:52 -0600

    CHANGED: Resolve ActiveRecord migratioon issue

--- test/orm/active_record.rb
+++ test/orm/active_record.rb
@@ -3,4 +3,9 @@ ActiveRecord::Base.logger = Logger.new(nil)
 
 migrations_path = File.expand_path("../../dummy/db/migrate/", __FILE__)
 
-ActiveRecord::MigrationContext.new(migrations_path, ActiveRecord::SchemaMigration).migrate
+if Rails.version.to_f >= 7.2
+  ActiveRecord::MigrationContext.new(migrations_path).migrate
+else
+  # To support order versions of Rails (pre v7.2)
+  ActiveRecord::MigrationContext.new(migrations_path, ActiveRecord::SchemaMigration).migrate
+end
