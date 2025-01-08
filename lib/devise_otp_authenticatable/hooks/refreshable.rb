670979cc4ff7efc4218aa2e11d0746e2b7a6c595
Chris Dyer <chris@cdyer.co.uk>
2025-01-08 10:00:35 +0000

    Fix checking if model has OTP enabled.

--- lib/devise_otp_authenticatable/hooks/refreshable.rb
+++ lib/devise_otp_authenticatable/hooks/refreshable.rb
@@ -1,6 +1,6 @@
 # After each sign in, update credentials refreshed at time
 Warden::Manager.after_set_user except: :fetch do |record, warden, options|
-  if record.class.method_defined?(:otp_credentials_refresh)
+  if defined?(record.class.otp_credentials_refresh)
     warden.session(options[:scope])["credentials_refreshed_at"] = (Time.now + record.class.otp_credentials_refresh)
   end
 end
