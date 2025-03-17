1b1753da684a69f5778680389511ad78751b99e3
Vasili Makouchyk <vasili.makouchyk@oysterhr.com>
2025-03-17 15:34:56 +0400

    Update admin migration in dummy app

---
+++ test/dummy/db/migrate/20250317113351_devise_otp_add_to_admins.rb
@@ -0,0 +1,28 @@
+class DeviseOtpAddToAdmins < ActiveRecord::Migration[7.0]
+  def self.up
+    change_table :admins do |t|
+      t.string    :otp_auth_secret
+      t.string    :otp_recovery_secret
+      t.boolean   :otp_enabled,          :default => false, :null => false
+      t.boolean   :otp_mandatory,        :default => false, :null => false
+      t.datetime  :otp_enabled_on
+      t.integer   :otp_failed_attempts,  :default => 0, :null => false
+      t.integer   :otp_recovery_counter, :default => 0, :null => false
+      t.datetime  :otp_recovery_requested_at
+      t.string    :otp_persistence_seed
+
+      t.string    :otp_session_challenge
+      t.datetime  :otp_challenge_expires
+    end
+    add_index :admins, :otp_session_challenge,  :unique => true
+    add_index :admins, :otp_challenge_expires
+  end
+  
+  def self.down
+    change_table :admins do |t|
+      t.remove :otp_auth_secret, :otp_recovery_secret, :otp_enabled, :otp_mandatory, :otp_enabled_on, :otp_session_challenge,
+          :otp_challenge_expires, :otp_failed_attempts, :otp_recovery_counter, :otp_persistence_seed, :otp_recovery_requested_at
+
+    end
+  end
+end
