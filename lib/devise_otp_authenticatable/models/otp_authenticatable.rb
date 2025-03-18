e04371d1fa45b279e9035dbe666d3c0a86dc1f36
Vasili Makouchyk <vasili.makouchyk@oysterhr.com>
2025-03-18 16:16:46 +0400

    Reinstall devise_otp in dummy app, fix missing and mistyped methods

--- lib/devise_otp_authenticatable/models/otp_authenticatable.rb
+++ lib/devise_otp_authenticatable/models/otp_authenticatable.rb
@@ -11,7 +11,7 @@ module Devise::Models
     module ClassMethods
       ::Devise::Models.config(self, :otp_authentication_timeout, :otp_drift_window, :otp_trust_persistence,
         :otp_mandatory, :otp_credentials_refresh, :otp_issuer, :otp_recovery_tokens,
-        :otp_controller_path, :otp_max_failed_attempts, :otp_recovery_timeout)
+        :otp_controller_path, :otp_max_failed_attempts, :otp_recovery_timeout, :otp_by_email_code_valid_for)
 
       def find_valid_otp_challenge(challenge)
         with_valid_otp_challenge(Time.now).where(otp_session_challenge: challenge).first
@@ -104,7 +104,7 @@ module Devise::Models
     alias_method :valid_otp_token?, :validate_otp_token
 
     def validate_otp_by_email(token, time = now)
-      return if otp_by_email_current_token_expired?(time)
+      return if otp_by_email_current_code_expired?(time)
       otp_by_email.verify(token, otp_by_email_counter)
     end
 
