e3b38a01561d54080ae8465d1a707e7ea92d7248
Laney Stroup <laney@stroupsolutions.com>
2026-08-18 20:39:41 +0000

    execute fresh run of db migrations, and update schema.rb to match default column order;

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,36 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_18_030305) do
+ActiveRecord::Schema[8.1].define(version: 2025_08_18_030305) do
   create_table "admins", force: :cascade do |t|
-    t.string "name"
+    t.string "authentication_token"
     t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "current_sign_in_at", precision: nil
+    t.string "current_sign_in_ip"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: nil
-    t.datetime "remember_created_at", precision: nil
-    t.integer "sign_in_count", default: 0
-    t.datetime "current_sign_in_at", precision: nil
-    t.datetime "last_sign_in_at", precision: nil
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.integer "failed_attempts", default: 0
-    t.string "unlock_token"
+    t.datetime "last_sign_in_at", precision: nil
+    t.string "last_sign_in_ip"
     t.datetime "locked_at", precision: nil
-    t.string "authentication_token"
+    t.string "name"
     t.string "otp_auth_secret"
-    t.string "otp_recovery_secret"
-    t.boolean "otp_enabled", default: false, null: false
-    t.boolean "otp_mandatory", default: false, null: false
-    t.datetime "otp_enabled_on", precision: nil
-    t.integer "otp_time_drift", default: 0, null: false
-    t.integer "otp_failed_attempts", default: 0, null: false
-    t.integer "otp_recovery_counter", default: 0, null: false
-    t.string "otp_persistence_seed"
-    t.string "otp_session_challenge"
     t.datetime "otp_challenge_expires", precision: nil
+    t.boolean "otp_enabled", default: false, null: false
+    t.datetime "otp_enabled_on", precision: nil
+    t.integer "otp_failed_attempts", default: 0, null: false
+    t.boolean "otp_mandatory", default: false, null: false
+    t.string "otp_persistence_seed"
+    t.integer "otp_recovery_counter", default: 0, null: false
+    t.string "otp_recovery_secret"
+    t.string "otp_session_challenge"
+    t.integer "otp_time_drift", default: 0, null: false
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "reset_password_sent_at", precision: nil
+    t.string "reset_password_token"
+    t.integer "sign_in_count", default: 0
+    t.string "unlock_token"
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["authentication_token"], name: "index_admins_on_authentication_token", unique: true
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["otp_challenge_expires"], name: "index_admins_on_otp_challenge_expires"
@@ -49,34 +49,34 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_18_030305) do
   end
 
   create_table "lockable_users", force: :cascade do |t|
-    t.string "name"
+    t.string "authentication_token"
     t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "current_sign_in_at", precision: nil
+    t.string "current_sign_in_ip"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: nil
-    t.datetime "remember_created_at", precision: nil
-    t.integer "sign_in_count", default: 0
-    t.datetime "current_sign_in_at", precision: nil
-    t.datetime "last_sign_in_at", precision: nil
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.integer "failed_attempts", default: 0
-    t.string "unlock_token"
+    t.datetime "last_sign_in_at", precision: nil
+    t.string "last_sign_in_ip"
     t.datetime "locked_at", precision: nil
-    t.string "authentication_token"
+    t.string "name"
     t.string "otp_auth_secret"
-    t.string "otp_recovery_secret"
-    t.boolean "otp_enabled", default: false, null: false
-    t.boolean "otp_mandatory", default: false, null: false
-    t.datetime "otp_enabled_on", precision: nil
-    t.integer "otp_time_drift", default: 0, null: false
-    t.integer "otp_failed_attempts", default: 0, null: false
-    t.integer "otp_recovery_counter", default: 0, null: false
-    t.string "otp_persistence_seed"
-    t.string "otp_session_challenge"
     t.datetime "otp_challenge_expires", precision: nil
+    t.boolean "otp_enabled", default: false, null: false
+    t.datetime "otp_enabled_on", precision: nil
+    t.integer "otp_failed_attempts", default: 0, null: false
+    t.boolean "otp_mandatory", default: false, null: false
+    t.string "otp_persistence_seed"
+    t.integer "otp_recovery_counter", default: 0, null: false
+    t.string "otp_recovery_secret"
+    t.string "otp_session_challenge"
+    t.integer "otp_time_drift", default: 0, null: false
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "reset_password_sent_at", precision: nil
+    t.string "reset_password_token"
+    t.integer "sign_in_count", default: 0
+    t.string "unlock_token"
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["authentication_token"], name: "index_lockable_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_lockable_users_on_email", unique: true
     t.index ["otp_challenge_expires"], name: "index_lockable_users_on_otp_challenge_expires"
@@ -86,23 +86,23 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_18_030305) do
   end
 
   create_table "non_otp_users", force: :cascade do |t|
-    t.string "name"
+    t.string "authentication_token"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "current_sign_in_at"
+    t.string "current_sign_in_ip"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.integer "failed_attempts", default: 0
-    t.string "unlock_token"
+    t.datetime "last_sign_in_at"
+    t.string "last_sign_in_ip"
     t.datetime "locked_at"
-    t.string "authentication_token"
+    t.string "name"
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.integer "sign_in_count", default: 0
+    t.string "unlock_token"
+    t.datetime "updated_at", null: false
     t.index ["authentication_token"], name: "index_non_otp_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_non_otp_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_non_otp_users_on_reset_password_token", unique: true
@@ -110,35 +110,35 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_18_030305) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.text "body"
     t.datetime "created_at", precision: nil, null: false
+    t.string "title"
     t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "rememberable_users", force: :cascade do |t|
-    t.string "name"
+    t.datetime "current_sign_in_at"
+    t.string "current_sign_in_ip"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0
-    t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "name"
     t.string "otp_auth_secret"
-    t.string "otp_recovery_secret"
-    t.boolean "otp_enabled", default: false, null: false
-    t.boolean "otp_mandatory", default: false, null: false
-    t.datetime "otp_enabled_on"
-    t.integer "otp_time_drift", default: 0, null: false
-    t.integer "otp_failed_attempts", default: 0, null: false
-    t.integer "otp_recovery_counter", default: 0, null: false
-    t.string "otp_persistence_seed"
-    t.string "otp_session_challenge"
     t.datetime "otp_challenge_expires"
+    t.boolean "otp_enabled", default: false, null: false
+    t.datetime "otp_enabled_on"
+    t.integer "otp_failed_attempts", default: 0, null: false
+    t.boolean "otp_mandatory", default: false, null: false
+    t.string "otp_persistence_seed"
+    t.integer "otp_recovery_counter", default: 0, null: false
+    t.string "otp_recovery_secret"
+    t.string "otp_session_challenge"
+    t.integer "otp_time_drift", default: 0, null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.integer "sign_in_count", default: 0
     t.index ["email"], name: "index_rememberable_users_on_email", unique: true
     t.index ["otp_challenge_expires"], name: "index_rememberable_users_on_otp_challenge_expires"
     t.index ["otp_session_challenge"], name: "index_rememberable_users_on_otp_session_challenge", unique: true
@@ -146,34 +146,34 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_18_030305) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "authentication_token"
     t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "current_sign_in_at", precision: nil
+    t.string "current_sign_in_ip"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: nil
-    t.datetime "remember_created_at", precision: nil
-    t.integer "sign_in_count", default: 0
-    t.datetime "current_sign_in_at", precision: nil
-    t.datetime "last_sign_in_at", precision: nil
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.integer "failed_attempts", default: 0
-    t.string "unlock_token"
+    t.datetime "last_sign_in_at", precision: nil
+    t.string "last_sign_in_ip"
     t.datetime "locked_at", precision: nil
-    t.string "authentication_token"
+    t.string "name"
     t.string "otp_auth_secret"
-    t.string "otp_recovery_secret"
-    t.boolean "otp_enabled", default: false, null: false
-    t.boolean "otp_mandatory", default: false, null: false
-    t.datetime "otp_enabled_on", precision: nil
-    t.integer "otp_time_drift", default: 0, null: false
-    t.integer "otp_failed_attempts", default: 0, null: false
-    t.integer "otp_recovery_counter", default: 0, null: false
-    t.string "otp_persistence_seed"
-    t.string "otp_session_challenge"
     t.datetime "otp_challenge_expires", precision: nil
+    t.boolean "otp_enabled", default: false, null: false
+    t.datetime "otp_enabled_on", precision: nil
+    t.integer "otp_failed_attempts", default: 0, null: false
+    t.boolean "otp_mandatory", default: false, null: false
+    t.string "otp_persistence_seed"
+    t.integer "otp_recovery_counter", default: 0, null: false
+    t.string "otp_recovery_secret"
+    t.string "otp_session_challenge"
+    t.integer "otp_time_drift", default: 0, null: false
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "reset_password_sent_at", precision: nil
+    t.string "reset_password_token"
+    t.integer "sign_in_count", default: 0
+    t.string "unlock_token"
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["otp_challenge_expires"], name: "index_users_on_otp_challenge_expires"
