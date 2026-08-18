2e01142dc1902d577dbe03c232c007d7f04de254
Laney Stroup <laney@stroupsolutions.com>
2026-08-18 22:21:27 +0000

    revert Rails version for schema.rb to Rails 8.0;

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_08_18_030305) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_18_030305) do
   create_table "admins", force: :cascade do |t|
     t.string "authentication_token"
     t.datetime "created_at", precision: nil, null: false
